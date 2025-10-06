f6a4bbdb99efd380406538864db0be13fd808763
amolthebest <amolthebest007@gmail.com>
2025-10-06 23:24:23 +0530

    initial commit and sqlalchemy installed

---
+++ backend/db/base_class.py
@@ -0,0 +1,13 @@
+from typing import any
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import as_declarative
+
+@as_declarative
+class Base:
+
+    id: any
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
